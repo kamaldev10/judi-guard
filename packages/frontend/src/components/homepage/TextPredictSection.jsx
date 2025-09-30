d08595fe57033b71adc17c4bad65264093b0a141
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2025-09-30 19:31:25 +0700

    refactor: remofe funfact section from predict result section

--- packages/frontend/src/components/homepage/TextPredictSection.jsx
+++ packages/frontend/src/components/homepage/TextPredictSection.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import TextPredictForm from "@/components/text-predict/TextPredictForm";
-import FunFacts from "@/components/text-predict/FunFacts";
 
 const TextPredictSection = () => {
   return (
     // Container utama dengan background putih dan padding
-    <div className="w-full bg-[#B9E6FD] sm:bg-linear-90 sm:from-[#B9E6FD] sm:to-slate-100   py-4 sm:py-8">
-      <div className="max-w-7xl mx-auto px-0 sm:px-6 lg:px-8">
+    <div className="w-full h-dvh bg-[#B9E6FD] sm:bg-linear-90 sm:from-[#B9E6FD] sm:to-slate-100 py-4 sm:py-8">
+      <div className="max-w-7xl mx-auto px-0 sm:px-6">
         {/* Judul Utama Section */}
         <div className="text-center ">
           <h1 className="text-3xl font-extrabold tracking-tight text-teal-700 sm:text-4xl lg:text-5xl">
@@ -16,10 +15,8 @@ const TextPredictSection = () => {
             Alat canggih untuk menganalisis dan memahami teks terkait perjudian.
           </p>
         </div>
-
         {/* Layout Grid Responsif */}
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-12 items-stretch">
-          <FunFacts />
+        <div>
           <TextPredictForm />
         </div>
       </div>

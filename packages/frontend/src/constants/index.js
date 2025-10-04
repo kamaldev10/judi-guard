beaec7699efe1be2148120553cf6452132688e1f
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2025-10-05 02:31:11 +0700

    feat: add testimonials data

--- packages/frontend/src/constants/index.js
+++ packages/frontend/src/constants/index.js
@@ -1,4 +1,4 @@
-export const TestimonialsData = [
+export const testimonialsData = [
   {
     id: 1,
     quote: "Judi Guard sangat membantu membersihkan kolom komentar...",
@@ -46,5 +46,59 @@ export const TestimonialsData = [
     title: "Backend Developer",
     avatarUrl: "https://i.pravatar.cc/100?u=fajar",
     rating: 5,
+  },
+];
+
+import {
+  AliImage,
+  FerdianImage,
+  RafiImage,
+  RinaImage,
+  SalwaImage,
+  SolihunImage,
+} from "@/assets/images";
+
+export const membersData = [
+  {
+    name: "Ferdian Sakti Sudrajat Akbar",
+    cohortID: "MC209D5Y0290",
+    university: "Universitas Hasyim Asy’ari Tebuireng Jombang",
+    role: "Machine Learning",
+    image: FerdianImage,
+  },
+  {
+    name: "Ali Musthafa Kamal",
+    cohortID: "FC844D5Y0671",
+    university: "Universitas Riau",
+    role: "FrontEnd BackEnd",
+    image: AliImage,
+  },
+  {
+    name: "Laras Almanna Salwa",
+    cohortID: "FC237D5X2356",
+    university: "Universitas Gadjah Mada",
+    role: "FrontEnd BackEnd",
+    image: SalwaImage,
+  },
+  {
+    name: "Rina Rismawati",
+    cohortID: "MC367D5X0835",
+    university: "Institut Teknologi Garut",
+    role: "Machine Learning",
+    image: RinaImage,
+  },
+  {
+    name: "Rafi Nanda Edtrian",
+    cohortID: "MC308D5Y1092",
+    university: "Universitas Riau",
+    role: "Machine Learning",
+    image: RafiImage,
+  },
+  {
+    name: "M. Sholihun",
+    cohortID: "FC127D5Y0208",
+    university: "Politeknik Negeri Bengkalis",
+    role: "FrontEnd BackEnd",
+    image: SolihunImage,
   },
 ];

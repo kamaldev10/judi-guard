c6df78f1aa8d9b1e93b05d2f7f4148223d6e1868
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2025-12-07 14:32:55 +0700

    refactor(fe): add modular Error Boundary components

---
+++ packages/frontend/src/components/error-boundary/AppErrorBoundary.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./ErrorFallback";
+
+const AppErrorBoundary = ({ children }) => {
+  return (
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => {
+        window.location.reload();
+      }}
+      onError={(error, info) => {
+        console.error("Unhandled error:", error, info);
+      }}
+    >
+      {children}
+    </ErrorBoundary>
+  );
+};
+
+export default AppErrorBoundary;

baeb089d53d01c7423a3c1926465338fdc4a47e3
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2025-12-07 16:25:41 +0700

    chore(fe): update alias config

--- packages/frontend/vite.config.js
+++ packages/frontend/vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
       "@images": path.resolve(__dirname, "./src/assets/images"),
       "@icons": path.resolve(__dirname, "./src/assets/icons"),
       "@": path.resolve(__dirname, "./src"),
+      "@components": "./src/components",
     },
   },
   root: ".",

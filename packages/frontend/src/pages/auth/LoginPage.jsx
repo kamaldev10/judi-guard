a60f3afc9bb351774c4a3cda920fa5f402b47de2
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2025-12-07 15:56:56 +0700

    feat(fe): add login form page

---
+++ packages/frontend/src/pages/auth/LoginPage.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Title } from "react-head";
+import { LogoWithSlogan } from "@/assets/images";
+import LoginForm from "@/components/auth/LoginForm";
+
+const Login = () => {
+  return (
+    <>
+      <Title>Login | Judi Guard</Title>
+      <div className="relative fade-in-transition min-h-screen flex items-center justify-center ">
+        <div className="absolute inset-0">
+          <div className="h-1/2 bg-blue-200"></div>
+          <div className="h-1/2 bg-blue-100"></div>
+        </div>
+
+        <img
+          src={LogoWithSlogan}
+          alt="Judi Guard Logo"
+          width={150}
+          height={150}
+          className="absolute top-5"
+        />
+
+        <div className="z-10 w-full max-w-md bg-transparent p-8">
+          <LoginForm />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;

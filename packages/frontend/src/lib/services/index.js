18675c30978aa37209ffdbf1f9146f26cae5a3fd
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2025-12-07 16:00:23 +0700

    remove unused files

--- packages/frontend/src/lib/services/index.js
+++
@@ -1,5 +0,0 @@
-// src/lib/services/index.js
-export * from "./auth/authApi";
-export * from "./user/userApi";
-export * from "./analysis/videoAnalysisApi";
-export * from "./predict/predictTextApi";
--- packages/frontend/src/stores/index.js
+++
@@ -1,5 +0,0 @@
-export * from "./auth/authStore";
-export * from "./predict/textPredictStore";
-export * from "./user/userStore";
-export * from "./analysis/videoAnalysisStore";
-export * from "./ui/uiStore";

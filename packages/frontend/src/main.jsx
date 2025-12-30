33ddb5e1ad8ffa7f472ac88ddd23682de4206597
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2025-12-31 03:10:31 +0700

    chore(fe): google oauth provider component move to google sign in component

--- packages/frontend/src/main.jsx
+++ packages/frontend/src/main.jsx
@@ -1,19 +1,10 @@
 // src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./style.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "@/components/layout/ThemeProvider";
-
-const googleClientId = import.meta.env.VITE_GOOGLE_SIGNIN_CLIENT_ID;
-
-if (!googleClientId) {
-  console.error(
-    "Error: VITE_GOOGLE_SIGNIN_CLIENT_ID tidak terdefinisi. Fitur Google Sign-In tidak akan berfungsi."
-  );
-}
 
 const rootElement = document.getElementById("root");
 
@@ -22,17 +13,7 @@ if (rootElement) {
     <React.StrictMode>
       <ThemeProvider>
         <Router>
-          {googleClientId ? (
-            <GoogleOAuthProvider clientId={googleClientId}>
-              <App />
-            </GoogleOAuthProvider>
-          ) : (
-            <div>
-              Konfigurasi Google Sign-In belum lengkap. Harap hubungi
-              administrator.
-              <App />
-            </div>
-          )}
+          <App />
         </Router>
       </ThemeProvider>
     </React.StrictMode>

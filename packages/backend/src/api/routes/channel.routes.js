cca2faaa189f78d8d1172ae3c21bd93a0f42c333
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2026-01-22 13:21:41 +0700

    feature(be) : add channel routes that is videos, search and :videoId/comments

--- packages/backend/src/api/routes/channel.routes.js
+++ packages/backend/src/api/routes/channel.routes.js
@@ -1,1 +1,15 @@
-// Manajemen channel YouTube oleh pengguna
+// /src/api/routes/channel.routes.js
+const express = require("express");
+const router = express.Router();
+const channelController = require("../controllers/channel.controller");
+const ensureYoutubeAccess = require("../middlewares/ensureYoutubeAccess");
+
+router.use(ensureYoutubeAccess);
+
+router.get("/", channelController.getMyVideos);
+
+router.get("/search", channelController.searchVideoById);
+
+router.get("/:videoId/comments", channelController.getVideoComments);
+
+module.exports = router;

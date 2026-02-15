8f8d7cc3d73f09f8a76846095bee4632fda72363
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2026-02-16 01:21:28 +0700

    feat(fe) : add shadcn components

---
+++ packages/frontend/src/components/ui/skeleton.jsx
@@ -0,0 +1,15 @@
+import { cn } from "@/lib/utils/index";
+
+function Skeleton({ className, ...props }) {
+  return (
+    <div
+      className={cn(
+        "animate-pulse rounded-md bg-gray-200/80 dark:bg-gray-700/50",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+export { Skeleton };

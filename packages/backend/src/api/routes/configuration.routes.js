cb15e0d0906f9e05d5c9fdd6fb121585bc4dd3cd
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2026-05-01 23:26:59 +0700

    feat(be): add ensure Yotube Access Middleware

--- packages/backend/src/api/routes/configuration.routes.js
+++ packages/backend/src/api/routes/configuration.routes.js
@@ -4,28 +4,20 @@ const router = express.Router();
 const isAuthenticated = require("../middlewares/isAuthenticated");
 const ensureYoutubeAccess = require("../middlewares/ensureYoutubeAccess");
 
-router.use(isAuthenticated);
+router.use(isAuthenticated, ensureYoutubeAccess);
 
 // Whitelist Routes
 router.post("/whitelist", ensureYoutubeAccess, configController.addWhitelist);
 
 router.get("/whitelist", configController.getWhitelist);
 
-router.delete(
-  "/whitelist/:id",
-
-  configController.deleteWhitelist,
-);
+router.delete("/whitelist/:id", configController.deleteWhitelist);
 
 // Blacklist Routes
 router.post("/blacklist", configController.addBlacklist);
 
 router.get("/blacklist", configController.getBlacklist);
 
-router.delete(
-  "/blacklist/:id",
-
-  configController.deleteBlacklist,
-);
+router.delete("/blacklist/:id", configController.deleteBlacklist);
 
 module.exports = router;

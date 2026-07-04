fbf57887cdcbba1d9d3d584f16e8188778869e30
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2026-07-04 23:24:18 +0700

    fix: add uptime and environtment in health route

--- packages/backend/src/api/routes/index.js
+++ packages/backend/src/api/routes/index.js
@@ -1,28 +1,33 @@
 // src/api/routes/index.js
-const express = require("express");
-const authRoutes = require("./auth.routes");
-const userRoutes = require("./user.routes");
-const videoAnalysisRoutes = require("./videoAnalysis.routes");
-const textPredictRoutes = require("./textPredict.routes");
-const studioRoutes = require("./studio.routes");
-const channelRoutes = require("./channel.routes");
-const configurationRoutes = require("./configuration.routes");
+const express = require('express');
+const authRoutes = require('./auth.routes');
+const userRoutes = require('./user.routes');
+const videoAnalysisRoutes = require('./videoAnalysis.routes');
+const textPredictRoutes = require('./textPredict.routes');
+const studioRoutes = require('./studio.routes');
+const channelRoutes = require('./channel.routes');
+const configurationRoutes = require('./configuration.routes');
 
 const router = express.Router();
 
 // Health check route
-router.get("/health", (req, res) => {
+router.get('/health', (req, res) => {
   res
     .status(200)
-    .json({ status: "UP", message: "Judi Guard API is healthy 💪" });
+    .json({
+      status: 'UP',
+      uptime: process.uptime(),
+      environment: process.env.NODE_ENV,
+      message: 'Judi Guard API is healthy 💪',
+    });
 });
 
-router.use("/auth", authRoutes);
-router.use("/users", userRoutes);
-router.use("/analysis", videoAnalysisRoutes);
-router.use("/studio", studioRoutes);
-router.use("/text", textPredictRoutes);
-router.use("/videos", channelRoutes);
-router.use("/config", configurationRoutes);
+router.use('/auth', authRoutes);
+router.use('/users', userRoutes);
+router.use('/analysis', videoAnalysisRoutes);
+router.use('/studio', studioRoutes);
+router.use('/text', textPredictRoutes);
+router.use('/videos', channelRoutes);
+router.use('/config', configurationRoutes);
 
 module.exports = router;

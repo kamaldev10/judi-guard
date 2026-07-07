3a56c0b6f3564dac5c3d95477dfcdd8e06c6bd6d
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2026-07-08 00:43:41 +0700

    chore(lint): update lint

--- packages/backend/src/config/database.js
+++ packages/backend/src/config/database.js
@@ -1,13 +1,13 @@
 // src/config/database.js
-const mongoose = require("mongoose");
-const config = require("./environment");
+const mongoose = require('mongoose');
+const config = require('./environment');
 
 const connectDB = async () => {
   try {
     await mongoose.connect(config.mongodbUri);
-    console.log("MongoDB Connected successfully! 🎉");
+    console.log('MongoDB Connected successfully! 🎉');
   } catch (err) {
-    console.error("MongoDB Connection Error 😟:", err.message);
+    console.error('MongoDB Connection Error 😟:', err.message);
     // Exit process with failure
     process.exit(1);
   }
